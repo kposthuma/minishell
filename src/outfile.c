3aafa1fcdb8d04c44a3680883962dac712f01ef5
Koen Posthuma <kposthum@f1r2s8.codam.nl>
2023-06-15 14:57:00 +0200

    in/outfile complaints

--- src/outfile.c
+++ src/outfile.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   inout.c                                            :+:    :+:            */
+/*   outfile.c                                          :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: kposthum <kposthum@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2023/06/13 14:06:30 by kposthum      #+#    #+#                 */
-/*   Updated: 2023/06/15 14:33:18 by kposthum      ########   odam.nl         */
+/*   Created: 2023/06/15 14:56:24 by kposthum      #+#    #+#                 */
+/*   Updated: 2023/06/15 14:56:33 by kposthum      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,37 +47,4 @@ t_outf	*has_outfile(t_commands *cmd)
 		outfile->append = true;
 	outfile->filename = ft_strdup(cmd->commands[cmd->comm_nr - 1][i + 1]);
 	return (outfile);
-}
-
-char	**trim_comm_in(char **comm)
-{
-	char	**comm_new;
-	size_t	i;
-
-	comm_new = ft_calloc(strofstrlen(comm) - 1, sizeof(char *));
-	i = 2;
-	while (comm[i])
-	{
-		comm_new[i - 2] = ft_strdup(comm[i]);
-		i++;
-	}
-	ft_free(comm);
-	return (comm_new);
-}
-
-t_inf	*has_infile(t_commands *cmd)
-{
-	t_inf	*infile;
-
-	if (cmd->commands[0][0][0] != '<')
-		return (NULL);
-	infile = ft_calloc(1, sizeof(t_inf));
-	if (ft_strncmp(cmd->commands[0][0], "<<<", 4) == 0)
-		infile->in_type = 2;
-	else if (ft_strncmp(cmd->commands[0][0], "<<", 3) == 0)
-		infile->in_type = 1;
-	else if (ft_strncmp(cmd->commands[0][0], "<", 2) == 0)
-		infile->in_type = 0;
-	infile->filename = ft_strdup(cmd->commands[0][1]);
-	return (infile);
 }
